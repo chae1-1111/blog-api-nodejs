010b3b530a475f15e69d44da2a1f16284f3f7d62
임채원 <imchaewon@imchaewon-ui-MacBookPro.local>
2022-06-14 20:28:43 +0900

    edit post router get post list to include replys number

--- controller/postCont.ts
+++ controller/postCont.ts
@@ -61,7 +61,7 @@ export const getPostList: Function = async (
                     UserId: userid,
                     Category: category === "" ? { $exists: true } : category,
                 },
-                "-_id Title Description Name Created PostKey Views Likes" +
+                "-_id Title Description Name Created PostKey Views Likes Replys" +
                     category ===
                     ""
                     ? " Category"
